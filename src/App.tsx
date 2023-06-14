46a65eae4cac3bf42d675ad399ca2c3bd16b5e06
Simon Gibbons <s.gibbons@blackbird.video>
2023-06-14 15:15:06 +0100

    Explicitly end Kratos session

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, {useEffect} from 'react';
 import './App.css';
 import { useAuth } from "react-oidc-context";
+import { AxiosError } from 'axios';
+import {Configuration, FrontendApi} from "@ory/client";
 
 function App() {
     const auth = useAuth();
@@ -12,6 +14,14 @@ function App() {
           prompt: "login",
         })}
     }, [auth]);
+
+    // End the Kratos session if we have a session with Hydra - so we are always
+    // prompted to login.
+    useEffect(() => {
+        if (auth.isAuthenticated) {
+            endKratosSession();
+        }
+    }, [auth])
 
     if (auth.isAuthenticated) {
       return <>
@@ -25,5 +35,34 @@ function App() {
 
     return <p>Loading....</p>
 }
+
+const endKratosSession = async () => {
+    const ory = new FrontendApi(
+        new Configuration({
+            baseOptions: {
+                withCredentials: true,
+            },
+            basePath: 'http://localhost:4000',
+        })
+    );
+
+    try {
+        // end the ory session
+        const { data: flow } = await ory.createBrowserLogoutFlow();
+        await ory.updateLogoutFlow({
+            token: flow.logout_token,
+        });
+    } catch (e) {
+        if (isAxiosError(e) && e.response?.status === 401) {
+            // There was no session on Ory to begin with, so we are definitely logged out.
+            return;
+        }
+        throw e;
+    }
+};
+
+const isAxiosError = (error: unknown): error is AxiosError => {
+    return !!(error && (error as AxiosError).isAxiosError);
+};
 
 export default App;
